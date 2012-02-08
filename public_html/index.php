ffffae73da80d5e2d4ed4346f1dbd5109524bb32
zonoise <zonoise@gmail.com>
2012-02-08 15:01:27 +0900

    add database init script

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__.'/silex.phar'; 
-require_once __DIR__.'../config/database.php' ;#Database setting info $db_config
+#require_once __DIR__.'../config/database.php' ;#Database setting info $db_config
+require_once __DIR__.'/../config/database.php' ;
 $app = new Silex\Application(); 
 $app['debug'] = true;
 
---
+++ script/init_db.php
@@ -0,0 +1,93 @@
+<?php
+require_once(__DIR__ . '/../config/database.php');
+
+$create_table1 = "
+create table if not exists 
+units
+(
+  id          int not null auto_increment,
+  name        text not null,
+  mineral     int,
+  gas         int,
+  hp          int,
+  sields      int,
+  armer       int,
+  supply      int,
+  buildtime   int, 
+  energy      int,
+  movement    int,
+  sight_range int,
+  image_url   text,
+  PRIMARY KEY (id)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8;";
+
+$create_table2 = "
+create table if not exists 
+armer_types
+(
+  id            int not null auto_increment,
+  name          text not null,
+  PRIMARY KEY (id)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8;";
+
+$create_table3 = "
+create table if not exists 
+armertype_unit
+(
+  id            int not null,
+  unit_id       int not null,
+  armortype_id  int not null,
+  PRIMARY KEY (id)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8;";
+
+$create_table4 = "
+create table if not exists 
+atacks
+(
+  id            int not null ,
+  unit_id       int not null,
+  discription   text,
+  
+  speed         float ,
+  sprash        float ,
+  base_damage   float ,
+  dps           float ,
+  ground        float ,
+  air           float ,
+  attack_range         float ,
+  growth_per_upgrade   int not null,
+  bonus_armer_type_id  int not null,
+  bonus_point   int not null,
+  image_url     text,
+  PRIMARY KEY (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+$create_table5 = "
+create table if not exists 
+abilities
+(
+  id          int not null,
+  name        text not null,
+  discription text,
+  image_url   text
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8";
+
+try {
+  $pdo = new PDO("mysql:host=".$db_config['host_name']."; dbname=".$db_config['db_name'],
+                 $db_config['uname'],
+                 $db_config['password']);
+
+  $result = $pdo->query("create schema if not exists ". $db_config['db_name']);
+  $result = $pdo->query("use ".$db_config['db_name']);
+
+  $result = $pdo->query($create_table1);
+  $result = $pdo->query($create_table2);
+  $result = $pdo->query($create_table3);
+  $result = $pdo->query($create_table4);
+  $result = $pdo->query($create_table5);
+  print_r($pdo->errorInfo());
+}catch(PDOException $e){
+  print_r($pdo->errorInfo());
+  echo 'Connection failed: ' . $e->getMessage();
+}?>
+
