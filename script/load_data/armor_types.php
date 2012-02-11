af7186dcb174b13f3c163c1998d92a8e296aa2b8
zonoise <zonoise@gmail.com>
2012-02-12 00:26:38 +0900

    modify initilize script

--- script/load_data/armor_types.php
+++ script/load_data/armor_types.php
@@ -4,12 +4,14 @@ try{
 
 $db=Db::pdo();
 
-$attr= array( 'Light','Armored','Biological','Mechanical',
+$attr= array('Light','Armored','Biological','Mechanical',
        'Psionic','Massive','Structure');
 
 $stmt = $db->prepare('insert into armor_types (id,name) values (?,?)');
 foreach($attr as $k => $v){
+  print($v);
   $stmt->execute(array($k,$v));
+  print_r($sth->errorInfo());
 }
 
 }catch(PDOException $e){
