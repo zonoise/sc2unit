b22d58431387bf73ca0b59ae935fc9ba9aba93ab
zonoise <zonoise@gmail.com>
2012-02-12 05:01:35 +0900

    modify initialize script

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 $db_config = array('host_name'=>'DUMMY_DB_HOSTNAME',
-                   'uname'=>'DUMMY_DB_NAME',
-                   'password'=>'DUMMY_DB_USERNAME',
-                   'db_name'=>'DUMMY_DB_PASSWORD');
+                   'uname'=>'DUMMY_DB_USERNAME',
+                   'password'=>'DUMMY_DB_PASSWORD',
+                   'db_name'=>'DUMMY_DB_NAME');
 ?>
