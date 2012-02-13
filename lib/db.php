5198254d9bd240899a0ac7ff76a4951638963595
zonoise <zonoise@gmail.com>
2012-02-13 18:01:29 +0900

    mod htaccess

--- lib/db.php
+++ lib/db.php
@@ -11,12 +11,15 @@ class Db {
   static public function pdo(){
     try{
       global $db_config;
-      $pdo = new PDO("mysql:host=".$db_config['host_name']."; dbname=".$db_config['db_name'],
+      $pdo = new PDO("mysql:host=".$db_config['host_name'].';port='.$db_config['port'] . "; dbname=".$db_config['db_name'],
+                   $db_config['uname'],
+                   $db_config['password']);
+      print_r("mysql:host=".$db_config['host_name'].';port='.$db_config['port'] . "; dbname=".$db_config['db_name'],
                    $db_config['uname'],
                    $db_config['password']);
       //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      //$pdo->query("use ".$db_config['db_name']);
+      $pdo->query("use ".$db_config['db_name']);
       return $pdo;
     }catch(PDOException $e){
        echo 'Connection failed: ' . $e->getMessage();
